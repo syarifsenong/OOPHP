b2b33581a240ae6ad586a8ea6c03921d021aa4aa
Syarif <syarif.nurhid@gmail.com>
2020-09-09 16:02:32 +0700

    echo buku baru

--- PropertyMethod/Coba.php
+++ PropertyMethod/Coba.php
@@ -13,15 +13,13 @@ public function halo(){
 	return "hello world <br>";
 }
 public function getBuku(){
+	//fungsi $this untuk mengambil isi dari properti 
+	// dari class yg bersangkutan ketika dibuat instance
 	return "Buku $this->nama, Harga $this->harga";
 }
 
 }
 //object instance dari class
-// $buku1 = new Buku();
-// $buku1->nama = "JAVA";
-// var_dump($buku1);
-
 $buku2 = new Buku();
 //set value properti langsung
 $buku2->nama = "CSS";
@@ -30,6 +28,12 @@ $buku2->pengarang = "Senong";
 $buku2->penerbit = "Media";
 var_dump($buku2);
 
+$buku3 = new Buku();
+//set value properti langsung
+$buku3->nama = "PHP";
+$buku3->harga = 30000;
+$buku3->pengarang = "Padika";
+$buku3->penerbit = "Gema";
 
 echo "<br>";
 echo "Buku : $buku2->nama, $buku2->pengarang";
@@ -37,4 +41,7 @@ echo "<br>";
 //manggil method
 echo $buku2->halo();
 echo $buku2->getBuku();
+echo "<br>";
+//simbol titik untuk menggabungkan 
+echo "Ini adalah " . $buku3->getBuku();
  ?>
